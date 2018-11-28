e9d8ee7a023d394aced80bdd6e2f556f56a4e569
Suraj Mothilal <Suraj@Surajs-Mac-mini.local>
2018-11-27 19:08:14 -0500

    Removed Comment Line

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,6 @@ class Home extends Component{
     render(){
         return(
             <div>
-                //Comment Line
                 <DispatcherTable />
                 <div style={styles.row}>
                 <Available />
